d8e413aaf3d4a05f5c3b8c4321f7d28d0cb7d607
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-06 23:10:43 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use dotenvy;
 use poise::serenity_prelude as serenity;
-static token: String = dotenvy::var("token").unwrap();
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 // User data, which is stored and accessible in all command invocations
@@ -26,6 +25,7 @@ async fn register(ctx: Context<'_>) -> Result<(), Error> {
 
 #[tokio::main]
 async fn main() {
+    let token: String = dotenvy::var("token").unwrap();
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: vec![age(), register()],
