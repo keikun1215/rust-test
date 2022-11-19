c892c38e75b9591d865fccf8260f86e05a7308f7
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-19 16:53:04 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 // User data, which is stored and accessible in all command invocations
 /// Displays your or another user's account creation date/
-static fw: Framework = Framework;
+static fw: Framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![ping(), addcmd()],
+            ..Default::default()
+        })
+        .token(dotenvy::var("token").unwrap())
+        .intents(serenity::GatewayIntents::non_privileged())
+        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));;
 #[poise::command(slash_command, prefix_command)]
 async fn ping(
     ctx: Context<'_>,
@@ -24,15 +31,6 @@ async fn addcmd(ctx: Context<'_>) -> Result<(), Error> {
 
 #[tokio::main]
 async fn main() {
-    let token: String = dotenvy::var("token").unwrap();
-    let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions {
-            commands: vec![ping(), addcmd()],
-            ..Default::default()
-        })
-        .token(token)
-        .intents(serenity::GatewayIntents::non_privileged())
-        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));
-    fw = framework;
-    framework.run().await.unwrap();
+    //let token: String = dotenvy::var("token").unwrap();
+    fw.run().await.unwrap();
 }
