5f2d2c45488935ead8520ca3e1c3ef7370c93542
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-19 12:47:30 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dotenvy;
+use tokio::io::AsyncRead;
 use tokio::io::AsyncReadExt;
 use tokio::sync::Mutex;
 use std::sync::Arc;
@@ -13,6 +14,7 @@ struct ShardManagerContainer;
 impl TypeMapKey for ShardManagerContainer {
     type Value = Arc<Mutex<ShardManager>>;
 }
+pub trait AsyncRead {}
 /// Displays your or another user's account creation date
 #[poise::command(slash_command, prefix_command)]
 async fn ping(
