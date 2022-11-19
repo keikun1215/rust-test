3fd347092496d70306a40b33da63749df27cad85
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-19 17:04:12 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 // User data, which is stored and accessible in all command invocations
 /// Displays your or another user's account creation date/
-static fw: Framework = poise::Framework::builder()
+static fw: Framework<U, E> = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: vec![ping(), addcmd()],
             ..Default::default()
         })
         .token(dotenvy::var("token").unwrap())
         .intents(serenity::GatewayIntents::non_privileged())
-        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));;
+        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));
 #[poise::command(slash_command, prefix_command)]
 async fn ping(
     ctx: Context<'_>,
 ) -> Result<(), Error> {
-    let shard = Framework.shard_manager(fw).runners.get(ctx.discord().shard_id);
+    let shard = fw.shard_manager().runners.get(ctx.discord().shard_id);
     ctx.say(&format!("🏓**Pong!**\nping:  {:?}", shard.latency)).await?;
     Ok(())
 }
