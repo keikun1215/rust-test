bac991930d2c6b9c624b86d4fa8a7b50ed7ea5ab
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-19 14:07:51 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,17 @@ use tokio::io::AsyncRead;
 use tokio::io::AsyncReadExt;
 use tokio::sync::Mutex;
 use std::sync::Arc;
-use poise::serenity_prelude::*;
-use poise::serenity_prelude::TypeMapKey;
-use poise::serenity_prelude::client::bridge::gateway::{ShardId, ShardManager};
+use poise::framework::Framework::client::*;
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 // User data, which is stored and accessible in all command invocations
-struct Data {}
-struct ShardManagerContainer;
-impl TypeMapKey for ShardManagerContainer {
-    type Value = Arc<Mutex<ShardManager>>;
-}
 /// Displays your or another user's account creation date
 #[poise::command(slash_command, prefix_command)]
 async fn ping(
     ctx: Context<'_>,
 ) -> Result<(), Error> {
-    let data = ctx.data().read().await;
-    let shard_manager = match data.get::<ShardManagerContainer>() {
-      Some(v) => v,
-      None => {
-        ctx.say("Some problem occurred").await?;
-        return Ok(());
-      }
-    };
-    let manager = shard_manager.lock().await;
-    let runners = manager.runners.lock().await;
-    let runner = match runners.get(&ShardId(ctx.discord().shard_id)) {
-      Some(runner) => runner,
-      None => {
-        ctx.say("Some problem occurred").await?;
-        return Ok(());
-      },
-    };
-    ctx.say(&format!("🏓**Pong!**\nping:  {:?}", runner.latency)).await?;
+    let shard = ShardManager().runners.get(ctx.discord().shard_id)
+    ctx.say(&format!("🏓**Pong!**\nping:  {:?}", shard.latency)).await?;
     Ok(())
 }
 
