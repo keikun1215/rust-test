eccfa3523548af9a5bf13ce4b601efe11fea5894
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-19 15:13:14 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 // User data, which is stored and accessible in all command invocations
 /// Displays your or another user's account creation date
+static fw: Framework;
 #[poise::command(slash_command, prefix_command)]
 async fn ping(
     ctx: Context<'_>,
 ) -> Result<(), Error> {
-    let shard = Framework::shard_manager().runners.get(ctx.discord().shard_id);
+    let shard = Framework::shard_manager(fw).runners.get(ctx.discord().shard_id);
     ctx.say(&format!("🏓**Pong!**\nping:  {:?}", shard.latency)).await?;
     Ok(())
 }
@@ -32,6 +33,6 @@ async fn main() {
         .token(token)
         .intents(serenity::GatewayIntents::non_privileged())
         .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));
-
+    fw = framework;
     framework.run().await.unwrap();
 }
